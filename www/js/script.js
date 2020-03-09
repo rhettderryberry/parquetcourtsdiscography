2efa32748aed862e926068a332c086ab627ac57a
rhettderryberry <57115462+rhettderryberry@users.noreply.github.com>
2020-03-09 16:58:38 -0500

    Updates script.js with first hide/show function

---
+++ www/js/script.js
@@ -0,0 +1,16 @@
+function myFunction1() {
+
+    document.getElementById("americanalbumid").style.display = "block";
+
+    document.getElementById("lightupalbumid").style.display = "none";
+
+    document.getElementById("sunbathalbumid").style.display = "none";
+
+    document.getElementById("contentalbumid").style.display = "none";
+
+    document.getElementById("humanalbumid").style.display = "none";
+
+    document.getElementById("widealbumid").style.display = "none";
+
+}
+
