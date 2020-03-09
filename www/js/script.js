b21701c6b3411824303994780c2146159448c8df
rhettderryberry <57115462+rhettderryberry@users.noreply.github.com>
2020-03-09 17:01:34 -0500

    Updates function naming on script.js

--- www/js/script.js
+++ www/js/script.js
@@ -1,4 +1,4 @@
-function myFunction1() {
+function hideShow1() {
 
     document.getElementById("americanalbumid").style.display = "block";
 
@@ -14,3 +14,82 @@ function myFunction1() {
 
 }
 
+function hideShow2() {
+
+    document.getElementById("americanalbumid").style.display = "block";
+
+    document.getElementById("lightupalbumid").style.display = "none";
+
+    document.getElementById("sunbathalbumid").style.display = "none";
+
+    document.getElementById("contentalbumid").style.display = "none";
+
+    document.getElementById("humanalbumid").style.display = "none";
+
+    document.getElementById("widealbumid").style.display = "none";
+
+}
+
+function hideShow3() {
+
+    document.getElementById("americanalbumid").style.display = "block";
+
+    document.getElementById("lightupalbumid").style.display = "none";
+
+    document.getElementById("sunbathalbumid").style.display = "none";
+
+    document.getElementById("contentalbumid").style.display = "none";
+
+    document.getElementById("humanalbumid").style.display = "none";
+
+    document.getElementById("widealbumid").style.display = "none";
+
+}
+
+function hideShow4() {
+
+    document.getElementById("americanalbumid").style.display = "block";
+
+    document.getElementById("lightupalbumid").style.display = "none";
+
+    document.getElementById("sunbathalbumid").style.display = "none";
+
+    document.getElementById("contentalbumid").style.display = "none";
+
+    document.getElementById("humanalbumid").style.display = "none";
+
+    document.getElementById("widealbumid").style.display = "none";
+
+}
+
+function hideShow5() {
+
+    document.getElementById("americanalbumid").style.display = "block";
+
+    document.getElementById("lightupalbumid").style.display = "none";
+
+    document.getElementById("sunbathalbumid").style.display = "none";
+
+    document.getElementById("contentalbumid").style.display = "none";
+
+    document.getElementById("humanalbumid").style.display = "none";
+
+    document.getElementById("widealbumid").style.display = "none";
+
+}
+
+function hideShow6() {
+
+    document.getElementById("americanalbumid").style.display = "block";
+
+    document.getElementById("lightupalbumid").style.display = "none";
+
+    document.getElementById("sunbathalbumid").style.display = "none";
+
+    document.getElementById("contentalbumid").style.display = "none";
+
+    document.getElementById("humanalbumid").style.display = "none";
+
+    document.getElementById("widealbumid").style.display = "none";
+
+}
