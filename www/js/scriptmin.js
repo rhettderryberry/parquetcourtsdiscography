1e76abc4142d35b76b404743df52295482f9b01b
rhettderryberry <57115462+rhettderryberry@users.noreply.github.com>
2020-03-22 21:46:23 -0500

    Reverts to previous commit, fixing button issue on contact page

---
+++ www/js/scriptmin.js
@@ -0,0 +1,6 @@
+var divs=["americanalbumid","lightupalbumid","sunbathalbumid","contentalbumid","humanalbumid","widealbumid"];var visibleDivId=null;function divVisibility(divId){if(visibleDivId===divId){visibleDivId=null}else{visibleDivId=divId}
+hideNonVisibleDivs()}
+function hideNonVisibleDivs(){var i,divId,div;for(i=0;i<divs.length;i++){divId=divs[i];div=document.getElementById(divId);if(visibleDivId===divId){div.style.display="block"}else{div.style.display="none"}}}
+var newsDivs=["parishshow","savageandmurphy","monasticliving","poissonrouge","golddiggers"];var newsVisibleDivId=null;function newsDivVisibility(newsDivId){if(newsVisibleDivId===newsDivId){newsVisibleDivId=null}else{newsVisibleDivId=newsDivId}
+newsHideNonVisibleDivs()}
+function newsHideNonVisibleDivs(){var newsI,newsDivId,newsDiv;for(newsI=0;newsI<newsDivs.length;newsI++){newsDivId=newsDivs[newsI];div=document.getElementById(newsDivId);if(newsVisibleDivId===newsDivId){div.style.display="block"}else{div.style.display="none"}}}
